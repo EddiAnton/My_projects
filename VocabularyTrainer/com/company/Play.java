6f89b2b0a851e9df300b4b1ad9ac1b47dbceae88
Eduard Anton <eduardanton78@gmail.com>
2019-10-21 22:59:31 +0700

    VocabularyTrainer

--- VocabularyTrainer/com/company/Play.java
+++ VocabularyTrainer/com/company/Play.java
@@ -1,12 +1,106 @@
 package com.company;
 
+import javax.swing.*;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 
-public class Play {
+// Учебная программа для тренировки и запоминания
+// слов английского языка
+
+// Разработал Эдуард Антон
+// 20 октября 2019
+
+public class Play extends JFrame implements ActionListener {
+
+    final String TITLE_OF_PROGRAM = "Vocabulary trainer";
+    final int START_LOCATION = 200;
+    final int WINDOW_WIDTH = 370;
+    final int WINDOW_HEIGHT = 450;
+
+    JLabel label;
+    JTextPane windowDialog;
+    JTextField answer;
+    SimpleAttributeSet styleAnswer;
+    JButton button1;
+    JButton button2;
+    Trainer trainer = new Trainer();
 
     public static void main(String[] args) throws FileNotFoundException {
-        Trainer trainer = new Trainer();
-        trainer.printRecordsAmount();
-        trainer.trainerStart();
+
+        new Play();
+    }
+
+    // конструктор для создания окна программы
+    Play() throws FileNotFoundException {
+        setTitle(TITLE_OF_PROGRAM);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(START_LOCATION, START_LOCATION, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+        // диалоговое окно
+        windowDialog = new JTextPane();
+        windowDialog.setContentType("text/html");
+        JScrollPane scrollBar = new JScrollPane(windowDialog);
+        windowDialog.setToolTipText("Набери \"exit\" для окончания и просмотра результата");
+
+        // прописываем порядок вывода сообщений от программы
+        styleAnswer = new SimpleAttributeSet();
+        StyleConstants.setItalic(styleAnswer, true);
+        StyleConstants.setForeground(styleAnswer, Color.blue);
+        StyleConstants.setAlignment(styleAnswer, StyleConstants.ALIGN_RIGHT);
+
+        // создаем панель и ее элементы (кнопки, окно ввода и т.д.)
+        label = new JLabel(trainer.printRecordsAmount());
+        //label.setHorizontalTextPosition(label.CENTER);
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+        answer = new JTextField();
+        answer.addActionListener(this);
+        answer.setToolTipText("Введи сюда перевод");
+        button1 = new JButton("Новое слово");
+        button1.addActionListener(this);
+        button1.setToolTipText("Нажми для выода слова");
+        button2 = new JButton("Проверить");
+        button2.addActionListener(this);
+        button2.setToolTipText("Нажми для проверки слова");
+
+        // собираем все элементы
+        panel.add(button1);
+        panel.add(answer);
+        panel.add(button2);
+        add(BorderLayout.NORTH, label);
+        add(BorderLayout.CENTER, scrollBar);
+        add(BorderLayout.SOUTH, panel);
+        setVisible(true);
+    }
+
+    // определяем метод обработки событий нажатия на кнопки
+    @Override
+    public void actionPerformed(ActionEvent event) {
+            try {
+                StyledDocument document = windowDialog.getStyledDocument();
+                if(event.getSource() == button1) {
+                    document.insertString(document.getLength(), "Trainer: " +
+                            trainer.trainerStart(answer.getText()) + "\n", styleAnswer);
+                }
+                if(event.getSource() == button2) {
+                    document.insertString(document.getLength(), answer.getText() + "\n",
+                            new SimpleAttributeSet());
+
+                    document.insertString(document.getLength(), "Trainer: " +
+                            trainer.getTranslate(answer.getText()) + "\n", styleAnswer);
+                }
+
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        // Очищаем поле ввода и фокусируем в него кусор
+        answer.setText("");
+        answer.requestFocusInWindow();
     }
 }
--- VocabularyTrainer/com/company/Trainer.java
+++ VocabularyTrainer/com/company/Trainer.java
@@ -5,50 +5,69 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Trainer {
-    private int records = 0;
+    private int wordCount = 0;
+    private int positiveAnswerCount = 0;
+    private int negativeAnswerCount = 0;
 
-    public void printRecordsAmount() throws FileNotFoundException {
+    public String printRecordsAmount() throws FileNotFoundException {
         Scanner scan = new Scanner(new File("/home/eduard/IdeaProjects/VocabularyTrainer/src/com/company/words.txt"));
-        while (scan.hasNextLine()){
+        while (scan.hasNextLine()) {
             scan.nextLine();
-            records++;
+            wordCount++;
         }
-        System.out.println("Тренажер содержит " + records + " слов");
         scan.close();
+
+        return "Тренажер содержит " + wordCount + " слов.";
     }
 
-    public void trainerStart() throws FileNotFoundException {
-        while (true){
-            Scanner scan = new Scanner(new File("/home/eduard/IdeaProjects/VocabularyTrainer/src/com/company/words.txt"));
-            int pick = (int)(Math.random() * records + 1);
-            for (int i = 0; i < pick - 1; i++){
-                scan.nextLine();
-            }
-            String[] record = scan.nextLine().split("-");
-            System.out.println("Перевод слова \"" + record[0] + "\"?");
-            String[] translate = record[1].toLowerCase().split("[( {,|.}?)]+");
-            Scanner scn = new Scanner(System.in);
-            String guess = scn.nextLine();
+    private String[] record;
+    private String[] translate;
 
-            boolean flag = false;
-            for (String s : translate) {
-                if (guess.equals(s)) {
-                    flag = true;
-                }
-            }
 
-            if(guess.equals("exit")){
-                break;
-            }else{
-                if (flag) {
-                //if(guess.toLowerCase().equals(record[1].toLowerCase())){
-                    System.out.println("Верно");
-                    System.out.println();
-                }else {
-                    System.out.println("Неверно, правильный ответ - " + record[1]);
-                    System.out.println();
-                }
+    // метод возвращающий из тестового файла в рандомном порядке
+    // слово на иностранном языке без перевода
+    public String trainerStart(String text) throws FileNotFoundException {
+        Scanner scan = new Scanner(new File("/home/eduard/IdeaProjects/VocabularyTrainer/src/com/company/words.txt"));
+        int pick = (int) (Math.random() * wordCount + 1);
+        for (int i = 0; i < pick - 1; i++) {
+            scan.nextLine();
+        }
+        record = scan.nextLine().split("-");
+        return "Перевод слова " + record[0] + "?";
+    }
+
+    // метод проверяющий правильность введенного перевода слова
+    public String getTranslate(String gues) {
+
+        String guess = gues;
+        boolean flag = false;
+        translate = record[1].toLowerCase().split("[( {,|.}?)]+");
+        String result = "";
+
+        for (String s : translate) {
+            if (guess.equals(s)) {
+                flag = true;
             }
         }
+        // при вводе "exit" показываем пользователю количество
+        // правильных/неправильных ответов и обнуляем счетчики
+        if (guess.equals("exit")) {
+            result += "Ваш результат: \n";
+            result += "правильных ответов - " + positiveAnswerCount + "\n";
+            result += "неправильных ответов - " + negativeAnswerCount + "\n";
+            positiveAnswerCount = 0;
+            negativeAnswerCount = 0;
+
+        } else {
+            if (flag) {
+                positiveAnswerCount++;
+                result += "Верно\n\n";
+            } else {
+                negativeAnswerCount++;
+                result += "Неверно, правильный ответ - " + record[1] + "\n";
+            }
+        }
+        return result;
     }
 }
+
